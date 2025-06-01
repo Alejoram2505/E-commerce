6fa451baae2901f5e65204bf2191d4fb9197ded5
Alejoram2505 <Alejoram2505@gmail.com>
2025-05-31 23:49:00 -0600

    se agregaron productos

--- src/data/products.js
+++ src/data/products.js
@@ -4,14 +4,30 @@ export const products = [
     name: 'PowerSun Pro',
     price: 89.99,
     discount: 119.99,
-    image: 'https://placehold.co/200x200',
+    image: 'https://picsum.photos/seed/p1/200/200',
+    images: [
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
     rating: 2
   },
   {
     id: 2,
     name: 'NomadPack 30L',
     price: 119.0,
-    image: 'https://placehold.co/200x200',
+    image: 'https://picsum.photos/seed/p3/200/200',
+    images: [
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
     rating: 1
   },
   {
@@ -19,7 +35,15 @@ export const products = [
     name: 'TrailCharge Mini',
     price: 49.5,
     discount: 69.99,
-    image: 'https://placehold.co/200x200',
+    image: 'https://picsum.photos/seed/p6/200/200',
+    images: [
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
     rating: 2
   },
   {
@@ -27,54 +51,263 @@ export const products = [
     name: 'PowerSun Pro',
     price: 199.99,
     discountPrice: 159.99,
-    image: 'https://picsum.photos/seed/powersun/300/200',
+    image: 'https://picsum.photos/seed/powersun/200/200',
     rating: 4,
+    images: [
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
   },
   {
     id: 5,
     name: 'SolarHub Mini',
     price: 89.99,
-    image: 'https://picsum.photos/seed/solarhub/300/200',
+    image: 'https://picsum.photos/seed/solarhub/200/200',
     rating: 3,
+    images: [
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
   },
   {
     id: 6,
     name: 'Nomad Light Kit',
     price: 129.99,
     discountPrice: 99.99,
-    image: 'https://picsum.photos/seed/nomadlight/300/200',
+    image: 'https://picsum.photos/seed/nomadlight/200/200',
     rating: 5,
+    images: [
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
   },
   {
     id: 7,
     name: 'EcoCamp Stove',
     price: 249.99,
-    image: 'https://picsum.photos/seed/stove/300/200',
+    image: 'https://picsum.photos/seed/stove/200/200',
     rating: 4,
+    images: [
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
   },
   {
     id: 8,
     name: 'Backpack Xplorer',
     price: 109.99,
     discountPrice: 89.99,
-    image: 'https://picsum.photos/seed/backpack/300/200',
+    image: 'https://picsum.photos/seed/backpack/200/200',
     rating: 5,
+    images: [
+    'https://picsum.photos/seed/p1/600/400',
+    'https://picsum.photos/seed/p2/600/400',
+    'https://picsum.photos/seed/p3/600/400',
+    'https://picsum.photos/seed/p4/600/400',
+    'https://picsum.photos/seed/p5/600/400',
+    'https://picsum.photos/seed/p6/600/400'
+    ],
   },
   {
   id: 9,
   name: 'PowerSun Pro',
   price: 199.99,
   discountPrice: 159.99,
-  image: 'https://picsum.photos/seed/powersun/300/200',
+  image: 'https://picsum.photos/seed/powersun/200/200',
   images: [
   'https://picsum.photos/seed/p1/600/400',
   'https://picsum.photos/seed/p2/600/400',
   'https://picsum.photos/seed/p3/600/400',
   'https://picsum.photos/seed/p4/600/400',
   'https://picsum.photos/seed/p5/600/400',
-  'https://picsum.photos/seed/p6/600/400'
+  'https://picsum.photos/seed/p7/600/400'
   ],
   rating: 4
-}
-
+  },
+  {
+    id: 10,
+    name: 'EcoFlash 200',
+    price: 59.99,
+    discountPrice: 49.99,
+    image: 'https://picsum.photos/seed/p10/200/200',
+    images: [
+      'https://picsum.photos/seed/p10/200/200',
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400'
+    ],
+    rating: 4
+  },
+  {
+    id: 11,
+    name: 'Nomad Kettle',
+    price: 39.99,
+    image: 'https://picsum.photos/seed/p11/200/200',
+    images: [
+      'https://picsum.photos/seed/p11/200/200',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
+    rating: 3
+  },
+  {
+    id: 12,
+    name: 'SolarPack Mini',
+    price: 89.99,
+    discountPrice: 69.99,
+    image: 'https://picsum.photos/seed/p12/200/200',
+    images: [
+      'https://picsum.photos/seed/p12/200/200',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400',
+      'https://picsum.photos/seed/p1/600/400'
+    ],
+    rating: 5
+  },
+  {
+    id: 13,
+    name: 'Compact Hammock',
+    price: 74.99,
+    image: 'https://picsum.photos/seed/p13/200/200',
+    images: [
+      'https://picsum.photos/seed/p13/200/200',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400',
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400'
+    ],
+    rating: 2
+  },
+  {
+    id: 14,
+    name: 'Explorer Thermo',
+    price: 59.99,
+    discountPrice: 44.99,
+    image: 'https://picsum.photos/seed/p14/200/200',
+    images: [
+      'https://picsum.photos/seed/p14/200/200',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400',
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400'
+    ],
+    rating: 4
+  },
+  {
+    id: 15,
+    name: 'TrailCharge V2',
+    price: 149.99,
+    image: 'https://picsum.photos/seed/p15/200/200',
+    images: [
+      'https://picsum.photos/seed/p15/200/200',
+      'https://picsum.photos/seed/p6/600/400',
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400'
+    ],
+    rating: 5
+  },
+  {
+    id: 16,
+    name: 'Scout Gear 3L',
+    price: 49.99,
+    image: 'https://picsum.photos/seed/p16/200/200',
+    images: [
+      'https://picsum.photos/seed/p16/200/200',
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400'
+    ],
+    rating: 3
+  },
+  {
+    id: 17,
+    name: 'PowerRugged XL',
+    price: 219.99,
+    discountPrice: 199.99,
+    image: 'https://picsum.photos/seed/p17/200/200',
+    images: [
+      'https://picsum.photos/seed/p17/200/200',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400'
+    ],
+    rating: 5
+  },
+  {
+    id: 18,
+    name: 'CamperStove Duo',
+    price: 109.99,
+    discountPrice: 89.99,
+    image: 'https://picsum.photos/seed/p18/200/200',
+    images: [
+      'https://picsum.photos/seed/p18/200/200',
+      'https://picsum.photos/seed/p3/600/400',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400',
+      'https://picsum.photos/seed/p1/600/400'
+    ],
+    rating: 4
+  },
+  {
+    id: 19,
+    name: 'Forest Mat Pro',
+    price: 69.99,
+    image: 'https://picsum.photos/seed/p19/200/200',
+    images: [
+      'https://picsum.photos/seed/p19/200/200',
+      'https://picsum.photos/seed/p4/600/400',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400',
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400'
+    ],
+    rating: 1
+  },
+  {
+    id: 20,
+    name: 'Beacon Lantern 360',
+    price: 89.99,
+    image: 'https://picsum.photos/seed/p20/200/200',
+    images: [
+      'https://picsum.photos/seed/p20/200/200',
+      'https://picsum.photos/seed/p5/600/400',
+      'https://picsum.photos/seed/p6/600/400',
+      'https://picsum.photos/seed/p1/600/400',
+      'https://picsum.photos/seed/p2/600/400',
+      'https://picsum.photos/seed/p3/600/400'
+    ],
+    rating: 2
+  }
 ]
